3757a0d06d126c8beb3890f32c2a6a0de8f38cf8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-03-27 21:44:46 -0300

    EacRest::Entity.from_array_data.

--- lib/eac_rest/entity.rb
+++ lib/eac_rest/entity.rb
@@ -6,5 +6,11 @@ module EacRest
   class Entity
     enable_simple_cache
     common_constructor :api, :data
+
+    class << self
+      def from_array_data(api, array_data, *args)
+        array_data.map { |item_data| new(api, item_data, *args) }
+      end
+    end
   end
 end

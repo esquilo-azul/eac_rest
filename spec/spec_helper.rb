3bf70328615894cef97d62d632df656ace39bce3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2021-07-25 13:30:16 -0300

    spec/**/*.rb: adapt to "eac_ruby_gem_support" '0.3.0'.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,4 @@
 # frozen_string_literal: true
 
-::RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.shared_context_metadata_behavior = :apply_to_host_groups
-
-  require 'eac_ruby_gem_support/rspec'
-  ::EacRubyGemSupport::Rspec.setup(::File.expand_path('..', __dir__), config)
-end
+require 'eac_ruby_utils/rspec/default_setup'
+::EacRubyUtils::Rspec.default_setup_create(::File.expand_path('..', __dir__))
